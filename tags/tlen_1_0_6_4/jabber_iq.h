ecc065c90612b268bec95b2a30f489c2d0fe33a0
the_leech <the_leech@3f195757-89ef-0310-a553-cc0e5972f89c>
2005-06-03 20:50:34 +0000

    made a copy

---
+++ tags/tlen_1_0_6_4/jabber_iq.h
@@ -0,0 +1,60 @@
+/*
+
+Jabber Protocol Plugin for Miranda IM
+Tlen Protocol Plugin for Miranda IM
+Copyright (C) 2002-2004  Santithorn Bunchua
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#ifndef _JABBER_IQ_H_
+#define _JABBER_IQ_H_
+
+#include "jabber_xml.h"
+
+typedef enum {
+	IQ_PROC_NONE,
+	IQ_PROC_GETREGISTER,
+	IQ_PROC_SETREGISTER,
+	IQ_PROC_GETVCARD,
+	IQ_PROC_SETVCARD,
+	IQ_PROC_GETSEARCH,
+	IQ_PROC_GETGROUPS,
+	IQ_PROC_GETROOMS
+} JABBER_IQ_PROCID;
+
+typedef void (*JABBER_IQ_PFUNC)(XmlNode *iqNode, void *usedata);
+
+void JabberIqInit();
+void JabberIqUninit();
+JABBER_IQ_PFUNC JabberIqFetchFunc(int iqId);
+void JabberIqAdd(unsigned int iqId, JABBER_IQ_PROCID procId, JABBER_IQ_PFUNC func);
+
+void JabberIqResultSetAuth(XmlNode *iqNode, void *userdata);
+void JabberIqResultGetRoster(XmlNode *iqNode, void *userdata);
+void TlenIqResultGetVcard(XmlNode *iqNode, void *userdata);
+void JabberIqResultSetSearch(XmlNode *iqNode, void *userdata);
+void TlenIqResultChatGroups(XmlNode *iqNode, void *userdata);
+void TlenIqResultChatRooms(XmlNode *iqNode, void *userdata);
+void TlenIqResultUserRooms(XmlNode *iqNode, void *userdata);
+void TlenIqResultUserNicks(XmlNode *iqNode, void *userdata);
+void TlenIqResultRoomSearch(XmlNode *iqNode, void *userdata);
+void TlenIqResultRoomInfo(XmlNode *iqNode, void *userdata);
+void TlenIqResultChatRoomUsers(XmlNode *iqNode, void *userdata);
+//void JabberIqResultSetPassword(XmlNode *iqNode, void *userdata);
+
+#endif
+
