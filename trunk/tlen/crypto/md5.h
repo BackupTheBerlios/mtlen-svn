7a33a4d2f9bdd5713b6b4a1084349adc653a0937
the_leech <the_leech@3f195757-89ef-0310-a553-cc0e5972f89c>
2007-08-25 19:37:31 +0000

    retrieve protocol version for online contacts copyright notes updated

--- trunk/tlen/crypto/md5.h
+++ trunk/tlen/crypto/md5.h
@@ -2,7 +2,8 @@
 
 Jabber Protocol Plugin for Miranda IM
 Tlen Protocol Plugin for Miranda IM
-Copyright (C) 2002-2007  Santithorn Bunchua, Piotr Piastucki
+Copyright (C) 2002-2007  Santithorn Bunchua
+Copyright (C) 2004-2007  Piotr Piastucki
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
